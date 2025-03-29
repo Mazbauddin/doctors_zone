46bcbba26c18b3ce6b4941a0bd92c5bdbc5289f3
Mazba Uddin <mazbauddin2010@gmail.com>
2025-03-30 01:09:19 +0600

    paragraph add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-<h1 className='text-green-500 font-extrabold'>Hello World</h1>
+<h1 className='text-green-500 font-extrabold'>Hello World2</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quia vel excepturi ut voluptatum quas at, reiciendis molestiae temporibus aut mollitia culpa minima quos impedit optio veritatis officia provident. Possimus?</p>
     </div>
   )
 }
