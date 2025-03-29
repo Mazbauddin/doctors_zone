cdd867b0e242f670e74f436c3a02e171f0d164d1
Mazba Uddin <mazbauddin2010@gmail.com>
2025-03-30 01:54:07 +0600

    Create a Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Login from './pages/Login'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-<h1 className='text-green-500 font-extrabold'>Hello World2</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quia vel excepturi ut voluptatum quas at, reiciendis molestiae temporibus aut mollitia culpa minima quos impedit optio veritatis officia provident. Possimus?</p>
+    <div className='mx-4 sm:mx-[10%]'>
+<Navbar/>
+<Routes>
+  <Route path='/' element={<Home />}/>
+  <Route path='/doctors' element={<Doctors />}/>
+  <Route path='/doctors/:speciality' element={<Doctors />}/>
+  <Route path='/about' element={<About />}/>
+  <Route path='/contact' element={<Contact />}/>
+  <Route path='/my-appointments' element={<MyAppointments />}/>
+  <Route path='/appointment/:docId' element={<Appointment />}/>
+  <Route path='/my-profile' element={<MyProfile />}/>
+  <Route path='/login' element={<Login />}/>
+ 
+</Routes>
     </div>
   )
 }
