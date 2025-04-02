13a399a6050b035de21418e9e15b0af269912401
Mazba Uddin <mazbauddin2010@gmail.com>
2025-04-03 02:16:38 +0600

    complete about us page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -21,6 +21,21 @@ const About = () => {
       <div className='text-xl my-4'>
         <p className='uppercase'>Why <span className='text-gray-700 font-semibold'>Choose Us</span></p>
       </div>
+
+      <div className='flex flex-col md:flex-row'>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Efficiency:</b>
+          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Convenience:</b>
+          <p>Access to a network of trusted healthcare professionals in your area.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+          <b>Personalization:</b>
+          <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
+        </div>
+      </div>
     </div>
   )
 }
