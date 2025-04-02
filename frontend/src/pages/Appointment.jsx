5c69f75266860885bdf7f66c5573e23524eb66cf
Mazba Uddin <mazbauddin2010@gmail.com>
2025-04-02 23:52:51 +0600

    left side image and right side content create complete

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -23,14 +23,18 @@ const Appointment = () => {
     docInfo && (
       <div>
         {/* -------------Doctors Details---------------- */}
-        <div>
+        <div className="flex flex-col sm:flex-row gap-4">
           <div>
-            <img src={docInfo.image} alt="" />
+            <img
+              className="bg-primary w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt=""
+            />
           </div>
-          <div>
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[80px] sm:mt-0">
             {/* ------------Doc Info : Name , Degree, Experience */}
-            <p>
-              {docInfo.name} <img src={assets.verified_icon} alt="" />
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name} <img className="w-5" src={assets.verified_icon} alt="" />
             </p>
             <div>
               <p>
