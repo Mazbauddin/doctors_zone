61675ce4524cdc78932f25ad84bdf10ec6be0a11
Mazba Uddin <mazbauddin2010@gmail.com>
2025-04-03 02:39:38 +0600

    create Login Page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+const [state, setState] = useState('Sign Up')
+
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+const [name, setName] = useState('')
+
+
+const onSubmitHandler = async (event) =>{
+  event.preventDefault()
+
+}
+
   return (
-    <div></div>
+    <form>
+
+    </form>
   )
 }
 
