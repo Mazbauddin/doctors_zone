8b835bedabff25b40690f1bf80ca377a3804247e
Mazba Uddin <mazbauddin2010@gmail.com>
2025-04-04 02:32:58 +0600

    all appointment data fetch complete

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -11,7 +11,26 @@ const MyAppointments = () => {
     <div>
       <p>My Appointments</p>
       <div>
-
+        {doctors.slice(0, 2).map((item, index) => (
+          <div key={index}>
+            <div>
+              <img src={item.image} alt="" />
+            </div>
+            <div>
+              <p>{item.name}</p>
+              <p>{item.speciality}</p>
+              <p>Address</p>
+              <p>{item.address.line1}</p>
+              <p>{item.address.line2}</p>
+              <p><span>Date & Time:</span> 25, July, 2025 | 8:30PM</p>
+            </div>
+            <div></div>
+            <div>
+              <button>Pay Online</button>
+              <button>Cancel appointment</button>
+              </div>
+          </div>
+        ))}
       </div>
     </div>
   )
